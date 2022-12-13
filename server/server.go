1ad8e9c4e829ea1cc10516673f257b77ee0b40da
fireyy <fireyy@gmail.com>
2022-12-13 15:01:11 +0800

    fix: api CORS bug

--- server/server.go
+++ server/server.go
@@ -234,10 +234,12 @@ func (s *Server) Run(tpl *TPLInfo) error {
 	// h := http.Handler(http.HandlerFunc(s.webHandle))
 	// enable cors
 	cs := cors.New(cors.Options{
-		AllowedHeaders: []string{"Cache-Control"},
-		AllowedMethods: []string{"POST", "GET", "OPTIONS"},
-		// AllowedOrigins:   []string{"http://127.0.0.1:8080"},
-		// AllowCredentials: true,
+		AllowedHeaders:   []string{"Cache-Control", "Content-Type"},
+		AllowedMethods:   []string{"POST", "GET", "OPTIONS"},
+		AllowCredentials: true,
+		AllowOriginFunc: func(origin string) bool {
+			return true
+		},
 	})
 	h := cs.Handler(http.HandlerFunc(s.webHandle))
 	//gzip
